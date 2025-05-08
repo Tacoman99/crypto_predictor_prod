3bdb340079faeda7ef44b090a02a64014f776da9
jorge <jorgepadres789@gmail.com>
2025-05-07 23:35:23 -0700

    predictor added

---
+++ services/predictor/src/predictor/names.py
@@ -0,0 +1,8 @@
+def get_experiment_name(
+    pair: str, candle_seconds: int, prediction_horizon_seconds: int
+) -> str:
+    """
+    Returns the experiment name for the given pair, candle seconds, and prediction horizon
+    seconds.
+    """
+    return f'{pair}_{candle_seconds}_{prediction_horizon_seconds}'
